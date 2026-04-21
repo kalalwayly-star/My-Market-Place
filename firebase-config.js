6ffa675cd928c2541002eff980cad24d6badfae5
Kaled Al <kalalwayly@gmail.com>
2026-04-21 12:52:40 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Firebase App & Database SDKs
+import { initializeApp } from "https://gstatic.com";
+import { getDatabase, ref, push, onValue, set, remove } 
+from "https://gstatic.com";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBT8jv057_JQL6pIUYk-U_LQ8uJHlFi-o",
+  authDomain: "kal-marketplace.firebaseapp.com",
+  projectId: "kal-marketplace",
+  storageBucket: "kal-marketplace.firebasestorage.app",
+  messagingSenderId: "745728416819",
+  appId: "1:745728416819:web:da2dfb86cc5b79fb0d1746",
+  measurementId: "G-FFHYQC4YJV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Export tools for your other files
+export { db, ref, push, onValue, set, remove };
