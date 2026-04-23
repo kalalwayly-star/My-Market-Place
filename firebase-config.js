c2f0009761dfd318afb910db1449f928a40db882
Kaled Al <kalalwayly@gmail.com>
2026-04-23 00:20:04 -0500

    Update Firebase configuration and API key

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,11 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase
 import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBT8jv057_JQL6pIUYk-U_LQ8uJHlFi-o",
+  apiKey: "YOUR_KEY",
   authDomain: "kal-marketplace.firebaseapp.com",
   databaseURL: "https://kal-marketplace-default-rtdb.firebaseio.com",
   projectId: "kal-marketplace",
-  storageBucket: "kal-marketplace.firebasestorage.app",
+  storageBucket: "kal-marketplace.appspot.com",
   messagingSenderId: "745728416819",
   appId: "1:745728416819:web:da2dfb86cc5b79fb0d1746"
 };
@@ -17,8 +17,6 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 
-// 🔥 IMPORTANT: must be after auth init
 setPersistence(auth, browserLocalPersistence);
 
 export { db, auth };
-
