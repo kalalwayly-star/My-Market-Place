25773d955c0e8dd07b5d3c37655175fb2e65b615
Kaled Al <kalalwayly@gmail.com>
2026-04-23 01:33:25 -0500

    Update Firebase imports and export additional functions

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase, ref, push, onValue, set, remove } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+import { getAuth, setPersistence, browserLocalPersistence } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyDBT8jv057_JQL6pIUYk-U_LQ8uJHlFi-o",  
+  apiKey: "AIzaSyDBT8jv057_JQL6pIUYk-U_LQ8uJHlFi-o",
   authDomain: "kal-marketplace.firebaseapp.com",
   databaseURL: "https://kal-marketplace-default-rtdb.firebaseio.com",
   projectId: "kal-marketplace",
-  storageBucket: "kal-marketplace.appspot.com",
+  storageBucket: "kal-marketplace.firebasestorage.app",
   messagingSenderId: "745728416819",
   appId: "1:745728416819:web:da2dfb86cc5b79fb0d1746"
 };
@@ -19,4 +22,5 @@ const auth = getAuth(app);
 
 setPersistence(auth, browserLocalPersistence);
 
-export { db, auth };
+// ✅ EXPORT EVERYTHING YOU USE
+export { db, auth, ref, push, onValue, set, remove };
