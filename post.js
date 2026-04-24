4a81fd8de0a40bc6173461f6c3dcead92be4ddff
Kaled Al <kalalwayly@gmail.com>
2026-04-24 13:04:33 -0500

    Refactor imports for Firebase modules in post.js

--- post.js
+++ post.js
@@ -1,5 +1,6 @@
-import { auth, db } from "./firebase-config.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+import { auth } from "./firebase-config.js";  // For Firebase Authentication
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js"; // Authentication functions
+import { db, ref, onValue } from "./firebase-config.js";  // For Firebase Realtime Database functions
 
 /* =======================
    GLOBAL
